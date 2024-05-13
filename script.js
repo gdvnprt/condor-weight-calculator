d8f6d77fc5072d089c41a789aba26037dab0a21d
Gavin Davenport <gavinkdavenport@gmail.com>
2024-05-12 23:58:26 -0700

    Build custom item function
    can now add a custom item that will fit into all calculations

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ const addItemListen = (element, array, modifier) => {
     });
 };
 
+
+//had to tweak slightly for custom items
+const addCustomListen = (element, array, modifier) => {
+    element.addEventListener('change', () => {
+        let weightValue = element.parentNode.children[3].value * element.parentNode.children[5].value * modifier;
+        let posArr = Array.prototype.slice.call(document.querySelectorAll("." + element.parentNode.classList[0]));
+        let position = posArr.indexOf(element.parentNode);
+        //for some reason only the first in the list has a bug that makes its position -1 when it should be 0
+        if (position == -1) {
+            position = 0;
+        };
+        addItem(array, position, weightValue);
+        displayWeight();
+    });
+};
+
 //Delete button removes section div and place in array
 const deleteItem = (array, position) => {
   array.splice(position, 1);
@@ -85,9 +101,9 @@ let counter = 0;
 const duplicateDiv = (div) => {
     let dupDiv = div.cloneNode(true);
     dupDiv.children[1].id = dupDiv.children[1].id + counter;
-    dupDiv.children[0].for = dupDiv.children[1].id;
+    dupDiv.children[0].htmlFor = dupDiv.children[1].id;
     dupDiv.children[3].id = dupDiv.children[3].id + counter;
-    dupDiv.children[2].for = dupDiv.children[3].id;
+    dupDiv.children[2].htmlFor = dupDiv.children[3].id;
     counter++;
     div.parentNode.appendChild(dupDiv);
     if (div.classList[0] === "arm-item-div") {
@@ -110,10 +126,73 @@ newItemBtn.forEach(el => el.addEventListener('click', () => {
 }));
 
 //custom item creates a div with user-input weight value and name
+const createCustom = (element) => {
+    const customDiv = document.createElement('div');
+    customDiv.classList.add(element.parentNode.previousElementSibling.firstElementChild.classList[0]);
+    element.parentNode.previousElementSibling.appendChild(customDiv);
+
+    
+    const nameInput = document.createElement('input');
+    nameInput.id = "custom-name" + counter;
+    nameInput.type = "text";
+    nameInput.placeholder = "Item";
+
+    const nameLabel = document.createElement('label');
+    nameLabel.htmlFor = nameInput.id;
+    nameLabel.textContent = "Name:"
+
+    customDiv.appendChild(nameLabel);
+    customDiv.appendChild(nameInput);
+
+    const weightInput = document.createElement('input');
+    weightInput.id = "custom-weight" + counter;
+    weightInput.type = "number";
+    weightInput.value = "0";
+
+    const weightLabel = document.createElement('label');
+    weightLabel.htmlFor = weightInput.id;
+    weightLabel.textContent = "Weight:"
+
+    customDiv.appendChild(weightLabel);
+    customDiv.appendChild(weightInput);
+
+    const quantInput = document.createElement('input');
+    quantInput.id = "custom-quantity" + counter;
+    quantInput.type = "number";
+    quantInput.value = "1";
+
+    const quantLabel = document.createElement('label');
+    quantLabel.htmlFor = quantInput.id;
+    quantLabel.textContent = "Quantity:"
+
+    customDiv.appendChild(quantLabel);
+    customDiv.appendChild(quantInput);
+    counter++;
+
+    const deleteCustom = document.createElement('button');
+    deleteCustom.classList.add("delete-item");
+    deleteCustom.textContent = "x";
+    customDiv.appendChild(deleteCustom);
+    
+
+    if(customDiv.classList[0] === "arm-item-div") {
+        addItem(armArray, document.querySelectorAll(".arm-item-div").length, 0)
+        addCustomListen(weightInput, armArray, 1);
+        addCustomListen(quantInput, armArray, 1);
+        addDeleteListen(deleteCustom, armArray);
+    } else {
+        addItem(basketArray, document.querySelectorAll(".basket-item-div").length, 0)
+        addCustomListen(weightInput, basketArray, 1.37);
+        addCustomListen(quantInput, basketArray, 1.37);
+        addDeleteListen(deleteCustom, basketArray);
+    };
+};
+
 const customItemBtn = document.querySelectorAll('.add-custom');
-newItemBtn.forEach(el => el.addEventListener('click', () => {
-    //functions here
+customItemBtn.forEach(el => el.addEventListener('click', () => {
+    createCustom(el);
 }));
+
 //weight info pushes to DOM
 const displayWeight = () => {
     let liftCapacity = JSON.parse(selectLift.value);
